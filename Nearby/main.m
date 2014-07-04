153154bff3d30347a46d4057a7d71b900db0dee8
Jayant Madugula <brashivis@gmail.com>
2014-07-03 22:33:06 -0500

    Initial Commit

---
+++ Nearby/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Nearby
+//
+//  Created by Jayant Madugula on 7/3/14.
+//  Copyright (c) 2014 Jayant Madugula. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JMAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JMAppDelegate class]));
+    }
+}
